d0b66b311a2114be412acd81c23e77022d546fd5
Sascha Dittmann <Sascha5812@gmx.de>
2019-09-21 01:02:00 +0200

    "az login" added

--- Adls2SqlDw/UploadToAdls1.sh
+++ Adls2SqlDw/UploadToAdls1.sh
@@ -4,4 +4,13 @@ cd $HOME/clouddrive
 
 export adls1name="<adls1-account-name>"
 
+#login to azure using your credentials
+az account show 1> /dev/null
+
+if [ $? != 0 ];
+then
+	az login
+fi
+
+# Upload to ADLS Gen1
 az dls fs upload -n $adls1name --source-path "$HOME/clouddrive/data/contosoretaildw-tables" --destination-path /contosoretaildw --overwrite
